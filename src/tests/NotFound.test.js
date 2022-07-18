1df084796d65f683f7fff5e3ab24f2abe93ec8ee
Solon <amaral.solon@hotmail.com>
2022-07-18 20:16:13 -0300

    Requisito 4 :memo:

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { NotFound } from '../pages';
+
+describe('test notfound', () => {
+  test('notfound image', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+    const notFoundImage = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(notFoundImage).toHaveAttribute(
+      'alt', 'Pikachu crying because the page requested was not found',
+    );
+  });
+
+  test('notfound text', () => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+    const notFoundText = screen.getByText('Page requested not found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
