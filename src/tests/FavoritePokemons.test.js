06fa31e4e82b289dc10f4b03ef65b7edb1928fa4
Solon <amaral.solon@hotmail.com>
2022-07-18 18:22:22 -0300

    Requisito 3 :memo:

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { FavoritePokemons } from '../pages';
+
+describe('Test favorite', () => {
+  test('link to favorites', () => {
+    render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
+    const favoritesText = screen.getByText('No favorite pokemon found');
+    expect(favoritesText).toBeInTheDocument();
+  });
+});
